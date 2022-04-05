1468f58e559c0ab3fed7cae69dd2ae170587f403
Brandon Calabrese <thirtyvirus1@live.com>
2022-04-05 16:06:53 -0400

    further work on the plugin

--- src/SurvivalUtilities.java
+++ src/SurvivalUtilities.java
@@ -1,7 +1,5 @@
 import events.ProjectileHit;
-import items.experience_bottle;
-import items.omlette;
-import items.pillow;
+import items.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -43,9 +41,11 @@ public class SurvivalUtilities extends JavaPlugin {
         //UberItems.putItem("empty_item", new empty_item(Material.DIAMOND, "Empty UberItem", UberRarity.COMMON,
         //        false, false, false, Collections.emptyList(), null));
 
+        // TODO add Atlas
+
         UberItems.putItem("pillow", new pillow(Utilities.getSkull("http://textures.minecraft.net/texture/3088895e90ea5899499e32a3176ecdacd88656cba734d1345175fd11f2844893"), "Pillow",
                 UberRarity.COMMON, false, false, true,
-                Collections.singletonList(new UberAbility("Well Rested", AbilityType.NONE, "When sleeping in a bed while holding this pillow, you regain health. Clear poison effect", 300)),
+                Collections.singletonList(new UberAbility("Well Rested", AbilityType.NONE, "When sleeping in a bed while holding this pillow, you regain health. Cure any active poison effects.", 300)),
                 new UberCraftingRecipe(Arrays.asList(
                         new ItemStack(Material.STRING, 8),
                         new ItemStack(Material.FEATHER, 16),
@@ -83,6 +83,43 @@ public class SurvivalUtilities extends JavaPlugin {
                         new ItemStack(Material.LAPIS_LAZULI),
                         new ItemStack(Material.LAPIS_LAZULI)), false, 1)));
 
+        // TODO add way to convert enchantments into experience bottles?
+
+        // TODO add livna (reverse anvil)
+
+        // TODO add backpack
+
+        // TODO add double backpack
+
+        // TODO add foods on a stick
+
+        UberItems.putItem("homemade_portal_frame", new homemade_portal_frame(Material.END_PORTAL_FRAME, "Homemade Portal Frame",
+                UberRarity.EPIC, true, true, false,
+                Collections.emptyList(),
+                new UberCraftingRecipe(Arrays.asList(
+                        new ItemStack(Material.END_STONE, 12),
+                        new ItemStack(Material.END_STONE, 12),
+                        new ItemStack(Material.END_STONE, 12),
+                        new ItemStack(Material.ENDER_PEARL, 16),
+                        new ItemStack(Material.NETHER_STAR),
+                        new ItemStack(Material.ENDER_PEARL, 16),
+                        new ItemStack(Material.END_STONE, 12),
+                        new ItemStack(Material.END_STONE, 12),
+                        new ItemStack(Material.END_STONE, 12)), false, 12)));
+
+        UberItems.putItem("crystal_ball", new crystal_ball(Utilities.getSkull("http://textures.minecraft.net/texture/5a5f29a76d1f91c165f63baac048670e7b1d37ce785a4d9c21d8c3a177b5"), "Crystal Ball",
+                UberRarity.EPIC, false, false, false,
+                Arrays.asList(new UberAbility("storage", AbilityType.NONE, "Store Experience in this orb")),
+                new UberCraftingRecipe(Arrays.asList(
+                        new ItemStack(Material.GLASS),
+                        new ItemStack(Material.SEA_LANTERN),
+                        new ItemStack(Material.GLASS),
+                        new ItemStack(Material.SEA_LANTERN),
+                        new ItemStack(Material.BEACON),
+                        new ItemStack(Material.SEA_LANTERN),
+                        new ItemStack(Material.GLASS),
+                        new ItemStack(Material.SEA_LANTERN),
+                        new ItemStack(Material.GLASS)), false, 1)));
 
     }
     private void registerUberMaterials() {
--- src/items/crystal_ball.java
+++ src/items/crystal_ball.java
@@ -4,11 +4,7 @@ import java.util.List;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.Sound;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
-import org.bukkit.block.data.BlockData;
-import org.bukkit.block.data.Directional;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -20,34 +16,19 @@ import org.bukkit.inventory.ItemStack;
 import thirtyvirus.uber.UberItem;
 import thirtyvirus.uber.helpers.*;
 
-public class homemade_portal_frame extends UberItem {
+public class crystal_ball extends UberItem {
 
-    public homemade_portal_frame(Material material, String name, UberRarity rarity, boolean stackable, boolean oneTimeUse, boolean hasActiveEffect, List<UberAbility> abilities, UberCraftingRecipe craftingRecipe) {
-        super(material, name, rarity, stackable, oneTimeUse, hasActiveEffect, abilities, craftingRecipe);
+    public crystal_ball(ItemStack itemStack, String name, UberRarity rarity, boolean stackable, boolean oneTimeUse, boolean hasActiveEffect, List<UberAbility> abilities, UberCraftingRecipe craftingRecipe) {
+        super(itemStack, name, rarity, stackable, oneTimeUse, hasActiveEffect, abilities, craftingRecipe);
     }
     public void onItemStackCreate(ItemStack item) { }
-    public void getSpecificLorePrefix(List<String> lore, ItemStack item) {
-        lore.add(ChatColor.GRAY + "Hand Crafted.");
-        lore.add(ChatColor.GRAY + "Flawlessly Designed.");
-        lore.add(ChatColor.GRAY + "Excessively Expensive.");
-    }
+    public void getSpecificLorePrefix(List<String> lore, ItemStack item) { }
     public void getSpecificLoreSuffix(List<String> lore, ItemStack item) { }
 
     public boolean leftClickAirAction(Player player, ItemStack item) { return false; }
     public boolean leftClickBlockAction(Player player, PlayerInteractEvent event, Block block, ItemStack item) { return false; }
     public boolean rightClickAirAction(Player player, ItemStack item) { return false; }
-    public boolean rightClickBlockAction(Player player, PlayerInteractEvent event, Block block, ItemStack item) {
-        Block relative = block.getRelative(event.getBlockFace());
-        if (relative.getType() == Material.AIR) {
-            relative.setType(Material.END_PORTAL_FRAME);
-            BlockData data = relative.getBlockData();
-            ((Directional)data).setFacing(player.getFacing().getOppositeFace());
-            relative.setBlockData(data);
-            player.playSound(relative.getLocation(), Sound.BLOCK_END_PORTAL_FRAME_FILL, 1, 0.3f);
-            return true;
-        }
-        return false;
-    }
+    public boolean rightClickBlockAction(Player player, PlayerInteractEvent event, Block block, ItemStack item) { return false; }
     public boolean shiftLeftClickAirAction(Player player, ItemStack item) { return false; }
     public boolean shiftLeftClickBlockAction(Player player, PlayerInteractEvent event, Block block, ItemStack item) { return false; }
     public boolean shiftRightClickAirAction(Player player, ItemStack item) { return false; }
